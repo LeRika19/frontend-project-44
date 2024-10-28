1c2de8249ea68303c54678bc16308d278f8a1ff2
lerikal <lerikal1990@gmail.com>
2024-10-28 18:07:40 +0300

    closed the fifth point Game: Check for parity

---
+++ bin/brain-even.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+// Функция для проверки на четность
+const isEven = (num) => num % 2 === 0;
+
+// Функция для запуска игры
+const playGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  let correctAnswers = 0;
+  const roundsToWin = 3;
+
+  // Игра продолжается до тех пор, пока пользователь не ответит правильно 3 раза
+  while (correctAnswers < roundsToWin) {
+    const randomNumber = Math.floor(Math.random() * 100) + 1; // Генерируем случайное число от 1 до 100
+    console.log(`Question: ${randomNumber}`);
+
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+    let correctAnswer;
+    if (isEven(randomNumber)) {
+      correctAnswer = 'yes';
+    } else {
+      correctAnswer = 'no';
+    }
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswers += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+
+  // Если игрок ответил правильно 3 раза подряд
+  console.log(`Congratulations, ${name}!`);
+};
+
+// Запускаем игру
+playGame();
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
-#!/usr/bin/env node 
-console.log("Welcome to the Brain Games!");
+#!/usr/bin/env node
+import greetUser from '../src/cli.js';
+
+greetUser();
