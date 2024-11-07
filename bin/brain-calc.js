2e1ae772832b38b69430ea094139325cff9c029c
lerikal <lerikal1990@gmail.com>
2024-11-07 16:51:56 +0300

    fixing bugs in files

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { brainCalcGame } from '../src/games/game-calc.js'
 
 brainCalcGame();
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { brainGcdGame } from '../src/games/game-gcd.js'
 
 brainGcdGame();
